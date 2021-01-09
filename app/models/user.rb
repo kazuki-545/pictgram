440c1feafeceef5f72ab319c0d477a344c4df587
EC2 Default User <ec2-user@ip-172-31-41-33.us-east-2.compute.internal>
2021-01-09 05:25:23 +0000

    user問題とmodelについて　sign in 改変

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximam:15} 
+  validates :email, presence: true, uniqueness:true
+  validates :password, presence: true, length: {minimum: 8, maximam:24} 
 
   
   has_secure_password
