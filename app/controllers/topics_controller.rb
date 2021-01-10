0d801a4e35fead38c6388b387a9f020e5bdab3b8
EC2 Default User <ec2-user@ip-172-31-41-33.us-east-2.compute.internal>
2021-01-10 06:01:50 +0000

    写真投稿機能の作成　完成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,15 @@
 class TopicsController < ApplicationController
+  def index
+    @topics = Topic.all
+  end
+  
   def new
     @topic = Topic.new
   end
   
   def create
     @topic = current_user.topics.new(topic_params)
-
+#binding.pry
     if @topic.save
       redirect_to topics_path, success: '投稿に成功しました'
     else
