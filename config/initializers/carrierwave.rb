ad6551daa7bf597ea1ba08fd35db8c39cf65070a
EC2 Default User <ec2-user@ip-172-31-41-33.us-east-2.compute.internal>
2021-02-06 05:27:18 +0000

    heroku02

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,15 +3,14 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  config.storage =:fog
+  config.storage :fog
   config.fog_provider = 'fog/aws'
+  config.fog_directory  = ENV['AWS_BUCKET']
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV['AKIAWYMWKRGSRGE53Y4B'],
-    aws_secret_access_key: ENV['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'],
-    region: ENV['ap-northeast-1'],
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    region: ENV['AWS_DEFAULT_REGION'],
     path_style: true
   }
-  config.fog_directory  = 'techboostbucket123'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/s3techboostbucket123'
 end
